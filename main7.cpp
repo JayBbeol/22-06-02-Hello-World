0f51e2d2c449f960ce02fb509ca68ec28f047777
JayBbeoL <wjdrn999@gmail.com>
2022-06-02 17:14:15 +0900

    정상현 22/06/02 Hello World!

---
+++ main7.cpp
@@ -0,0 +1,102 @@
+/*
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+	/*
+	{
+		cout << "     * * * *       " << endl;
+		cout << "     *     *       " << endl;
+		cout << "     *     *       " << endl;
+		cout << "     *     *       " << endl;
+		cout << "*                 *" << endl;
+		cout << "*                 *" << endl;
+		cout << "*                 *" << endl;
+		cout << "*                 *" << endl;
+		cout << "*                 *" << endl;
+		cout << "* * * * * * * * * *" << endl;
+
+
+
+		int A, B = {0};
+
+		cout << "A is ?";
+		cin >> A;
+		cout << "B is ?";
+		cin >> B;
+
+		cout << "16진수 (A + B) = " << hex << uppercase << A + B << endl;
+		*/
+
+
+		/*//A-B가 0보다 크면 true, 0보다 작으면 false
+		int A, B = { 0 };
+		bool result = false;
+
+
+
+		cout << "A is ?";
+		cin >> A;
+		cout << "B is ?";
+		cin >> B;
+
+		result = (A - B > 0) ? true : false;
+
+
+		cout << "result = " << boolalpha << result << boolalpha << endl;*/
+
+
+		/*
+		int a, b, c = { 0 };
+
+		cout << "a is ? ";
+		cin >> a;
+		cout << "b is ? ";
+		cin >> b;
+		cout << "c is ? ";
+		cin >> c;
+
+		if (a + b > c)
+		{
+			cout << a + b << endl;
+		}
+		else
+		{
+			cout << c << endl;
+		}
+	}
+	*/
+	/*
+	int a = 0b10101010; // 2진법 표기법 ( 앞에 0b 붙이기 )
+	int b = 0111; // 8진법 표기법 ( 앞에 0 붙이기 )
+	int c = 0xff; // 16진법 표기법 ( 앞에 0x 붙이기)
+
+
+	cout <<  a + b - c << endl;
+	*/
+	/*
+	int a, b, c = { 0 };
+
+
+	cout << "a is ? ";
+	cin >> a;
+	cout << "b is ? ";
+	cin >> b;
+	cout << "c is ? ";
+	cin >> c;
+
+	if (c > a && a > b)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+
+	*/
+
+
+	
---
+++ main8.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{/*
+	int Score = { 0 };
+	cin >> Score;
+
+	// 100~90 : A
+	// 90~80 : B
+	// 80~70 : C
+	// 70~60 : D
+	// 60~50 : E
+	// 나마지 점수 : F
+	
+	if (Score >= 90)
+	{
+		cout << "A" << endl;
+	}
+	else if (90 > Score && Score >= 80)
+	{
+		cout << "B" << endl;
+	}
+	else if (80 > Score && Score >= 70)
+	{
+		cout << "C" << endl;
+	}
+	else if (70 > Score && Score >= 60)
+	{
+		cout << "D" << endl;
+	}
+	else if (60 > Score && Score >= 50)
+	{
+		cout << "E" << endl;
+	}
+	else
+	{
+		cout << "F" << endl;
+	}
+}
+
+int main()
+{
+	int Score = { 0 };
+	cin >> Score;
+
+	------------------------------------------
+
+	Score /= 10;
+
+	switch (Score)
+	{
+	case 10 :
+		__fallthrough;
+	case 9:
+		cout << "B" << endl;
+		break;
+	case 8:
+		cout << "C" << endl;
+		break;
+	case 7:
+		cout << "D" << endl;
+		break;
+	case 6:
+		cout << "E" << endl;
+		break;
+	case 5:
+		cout << "F" << endl;
+		break;
+	}
+
+	
+	-----------------------------------------------
+
+	int a, b = { 0 };
+	cout << "A is ? ";
+	cin >> a;
+	cout << "B is ? ";
+	cin >> b;
+
+	bool result = (a - b > 0);
+	
+	switch (result)
+	{
+	case true:
+		cout << "True" << endl;
+		break;
+	default:
+		cout << "False" << endl;
+		break;
+	}
+	----------------------------------------
+	
+	int a, b, c = { 0 };
+
+	cout << "A is ? ";
+	cin >> a;
+	cout << "B is ? ";
+	cin >> b;
+	cout << "C is ? ";
+	cin >> c;
+
+	bool result = (a + b > c);
+	
+	switch (result)
+	{
+	case true :
+		cout << a + b << endl;
+		break;
+	case false :
+		cout << c << endl;
+		break;
+	}
+	
+-----------------------------------------------
+
+
+int a = { 0 };
+
+
+cout << "What color?\n";
+cin >> a;
+
+switch (a)
+{
+case 1 :
+	cout << "Red" << endl;
+	break;
+case 2 :
+	cout << "Blue" << endl;
+	break;
+case 3 :
+	cout << "Black" << endl;
+	break;
+case 4 :
+	cout << "White" << endl;
+	break;
+}
+----------------------------------------------------
+
+
+int a = { 0 };
+cout << "주민번호 뒤에 7자리를 입력?" << endl;
+cin >> a;
+
+a /= 1000000;
+
+switch (a)
+{
+case 1 :
+	cout << "남자" << endl;
+	break;
+case 3 :
+	cout << "남자" << endl;
+	break;
+case 2 :
+	cout << "여자" << endl;
+	break;
+case 4:
+	cout << "여자" << endl;
+	break;
+default:
+	cout << "외계인" << endl;
+	break;
+
+}
+	-----------------------------------------------
+	*/
+
+
+}
